0d93ffc6bf10b4d76cf1e13c42c2c92afb9e2e4e
mgh3326 <mgh3326@naver.com>
2018-01-16 16:32:57 +0900

    complete

--- Intermediate_Algorithm_Scripting_Diff_Two_Arrays.js
+++ Intermediate_Algorithm_Scripting_Diff_Two_Arrays.js
@@ -1,15 +1,22 @@
-
 function diffArray(arr1, arr2) {
     var newArr = [];
     // Same, same; but different.
-    for(var i=0;i<arr2.length;i++)
-    {
-        if(arr1.indexOf(arr2[i])===-1)
-        {
-            console.log(arr2[i]);
+    for (var i = 0; i < arr2.length; i++) {
+        // console.log(arr1.indexOf(arr2[i]));
+        if (arr1.indexOf(arr2[i]) === -1) {
+            console.log(arr2.slice(i, i + 1));
+            newArr = newArr.concat(arr2.slice(i, i + 1))
         }
 
+
     }
+    for (i = 0; i < arr1.length; i++) {
+        if (arr2.indexOf(arr1[i]) === -1) {
+            console.log(arr1.slice(i, i + 1));
+            newArr = newArr.concat(arr1.slice(i, i + 1))
+        }
+    }
+
     // for(i=0;i<arr1.length;i++)
     // {
     //     if(arr2.indexOf(arr1[i])===-1)
@@ -18,7 +25,8 @@ function diffArray(arr1, arr2) {
     //     }
     //
     // }
+    console.log(newArr);
     return newArr;
 }
 
-diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+diffArray([1, "calf", 3, "piglet"], [7, "filly"]);
